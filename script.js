ae934d530509f86f7f139389f52e8b6555b656f4
novaoneu <nova.on.tiktokk@gmail.com>
2025-02-16 10:52:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Website loaded!");
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+    } else {
+        sidebar.style.width = '250px';
+    }
+}
