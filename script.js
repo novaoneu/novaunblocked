44101c632ed6ad544b9b901d0f9de19df63a66b0
novaoneu <nova.on.tiktokk@gmail.com>
2025-02-16 11:12:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function toggleSidebar() {
         sidebar.style.width = '250px';
     }
 }
+
+document.querySelector('.dropbtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
